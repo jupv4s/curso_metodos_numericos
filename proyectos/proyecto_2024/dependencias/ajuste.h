ac9ef2e7a3ccfce90582fb9b18dbf7ccbe0f18ba
Julio Pérez V. <90805375+jupv4s@users.noreply.github.com>
2025-07-30 13:58:07 -0400

    Add quadratic regression implementation

--- proyectos/proyecto_2024/dependencias/ajuste.h
+++ proyectos/proyecto_2024/dependencias/ajuste.h
@@ -70,4 +70,44 @@ vector<double> Ajuste<T>::lineal()
     return constantes;
 }
 
+// Ajuste cuadrático (polinomio de grado 2) para datos de una sola variable
+template <class T>
+vector<double> Ajuste<T>::cuadratica()
+{
+    Matriz<double> m_dataset(v_dataset);
+
+    if (m_dataset.ncol() < 2)
+    {
+        return {};
+    }
+
+    // Matriz de diseño con columnas [1, x, x^2]
+    Matriz<double> X(m_dataset.nrow(), 3);
+
+    for (int i = 0; i < m_dataset.nrow(); i+=1)
+    {
+        double x = m_dataset(i, 0);
+        X(i, 0) = 1.0;
+        X(i, 1) = x;
+        X(i, 2) = x * x;
+    }
+
+    // Vector columna de la variable dependiente
+    Matriz<double> y(m_dataset.nrow(), 1);
+
+    for (int i = 0; i < m_dataset.nrow(); i+=1)
+    {
+        y(i,0) = m_dataset(i, 1);
+    }
+
+    Matriz<double> constantes_matriz = (inversa(transpuesta(X)*X)*transpuesta(X))*y;
+    vector<double> constantes;
+
+    for (int i = 0; i < 3; i+=1)
+    {
+        constantes.push_back(constantes_matriz.get()[i][0]);
+    }
+    return constantes;
+}
+
 #endif
